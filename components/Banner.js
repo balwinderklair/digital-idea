d160ac9fb34a04dc0853001c809516f678a3c628
balwinderklair <bklair16@gmail.com>
2021-04-03 09:45:31 +0000

    Initial commit

---
+++ components/Banner.js
@@ -0,0 +1,18 @@
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Hi, I'm Balwinder Klair</h1>
+            </header>
+            <div className="content">
+                <p>I am graphic designer and
+                this is my portfolio.</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
